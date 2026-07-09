6b544b69e2cdfe86d07104fea5a2f66957364ec8
Izz Abdallah <x3zoabdullah@gmail.com>
2026-07-09 15:20:22 +0400

    feat: mount auth and project routers in app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,23 @@
 import dotenv from "dotenv";
 dotenv.config()
 import express from 'express'
+import './config/passport.js'
+import { evaluationRouter } from "./routes/evaluation.routes.js";
+import { authRouter } from "./routes/auth.routes.js";
+import { projectRouter } from "./routes/project.routes.js";
+
 
 const app = express()
 
-app.get("/",(req,res) =>{
-    res.send("Hello World")
-})
+ 
+app.use(express.json())
 
-const PORT = process.env.PORT
+app.use('/api',evaluationRouter)
+app.use('/api/auth',authRouter)
+app.use('/api',projectRouter)
+
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost: ${PORT}`)
